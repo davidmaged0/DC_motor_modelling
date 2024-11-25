fb551d8e35a3c95e643e4abad89e95281c7235e5
David Maged <davidmaged1123@gmail.com>
2024-11-25 21:43:30 +0200

    PI controller added

--- MotorValues.m
+++ MotorValues.m
@@ -1,9 +1,10 @@
 Va = 150 ;
-Ra =0.4;
+Ra =0.2;
 B= 0.02;%Armature resistance 
-Tl = 30;% load torque 
+Tl = 00;% load torque 
 J =7 ;
-La =0.1;
-K =1.4;
+%La =0.1;
+La=0;
+K =0.55;
 radian2degree = 60/(2*pi);
 %first branch >>branch1
